3bd87d5719675b39fc3caadfcd1ef40386142f2b
M Rajesh <m.rajeshrajz@gmail.com>
2020-04-20 23:08:59 +0530

    added column chart demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import {MaterialModule} from './material/material.module';
 import { TournmentComponent } from './ipl/tournment/tournment.component';
 import { TeamsComponent } from './ipl/teams/teams.component';
 import { PlayersComponent } from './ipl/players/players.component';
-
+import { HighchartsChartModule } from 'highcharts-angular';
 
 @NgModule({
   declarations: [
@@ -30,7 +30,8 @@ import { PlayersComponent } from './ipl/players/players.component';
     MaterialModule,
     HttpClientModule,
     DefaultModule,
-    MaterialModule
+    MaterialModule,
+    HighchartsChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ipl/ipl.module.ts
+++ src/app/ipl/ipl.module.ts
@@ -1,7 +1,5 @@
-import { MaterialModule } from 'src/app/material/material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 
 @NgModule({
   declarations: [],
